71e8fe5592f28d289ca2a8968e23110f1ef205b0
Charles Mitchell <chuckmitchell@gmail.com>
2010-01-31 19:05:00 -0500

    Renaming viewers to  members

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,3 @@
 class Member < ActiveRecord::Base
+  has_many_and_belongs_to :viewings
 end
--- app/models/viewing.rb
+++ app/models/viewing.rb
@@ -1,2 +1,4 @@
 class Viewing < ActiveRecord::Base
+  has_many_and_belongs_to :members
+  has_many :films
 end
