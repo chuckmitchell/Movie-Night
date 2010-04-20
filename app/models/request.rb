cf23b8e751bae2630b1e0f3e75476c30980f6619
Charles Mitchell <chuckmitchell@gmail.com>
2010-04-19 21:30:51 -0400

    Request email and imdb validation

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,14 @@
 class Request < ActiveRecord::Base
 
+  url_regex = /http:\/\/www.imdb.com\/title\/.{9}\//ix
+  email_regex = /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i
+  
   belongs_to :film  
-  validates_presence_of     :requester_name, :requester_email, :imdb_permalink
+  validates_presence_of :requester_name, :requester_email, :imdb_permalink
+  validates_format_of :imdb_permalink, :with=>url_regex
+  validates_format_of :requester_email, :with=>email_regex
+  
+  
   
   
 end
